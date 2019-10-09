de4de7644eb9cc9f87e25515f83a956df8126ece
alptra <novannanega@gmail.com>
2019-10-09 11:22:27 +0700

    design dashboard bottombar

--- app/src/main/java/com/example/agentmedia/MainActivity.java
+++ app/src/main/java/com/example/agentmedia/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.agentmedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.agentmedia.fragments.DashboardFragment;
+import com.example.agentmedia.fragments.ProfilFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new DashboardFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.dashboard:
+                fragment = new DashboardFragment();
+                break;
+            case R.id.profil:
+                fragment = new ProfilFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
